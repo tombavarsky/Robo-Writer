ff983936f3f3751f708dcde2a37be6e8fc80162e
Tom Bavarsky <tomba16062004@gamil.com>
2020-11-10 22:52:25 +0200

    digit zero

--- src/digit.h
+++ src/digit.h
@@ -22,11 +22,26 @@ public:
 
 class Zero : public Digit
 {
+    static const int digit_length = 5;
+
 public:
     Zero() : Digit(0)
     {
-        const int digit_length = get_digit_length();
-        Point *draw_points = (Point *)malloc(digit_length * sizeof(Point));
-        draw_points[0] = Point(place_constant * position, top_y);
+        Point *draw_points = new Point[digit_length];
+        draw_points[0] = Point(place_constant, top_y);              //top left
+        draw_points[1] = Point(right_x + place_constant, top_y);    //top right
+        draw_points[2] = Point(right_x + place_constant, bottom_y); //bottom right
+        draw_points[3] = Point(place_constant, bottom_y);           //bottom left
+        draw_points[4] = Point(place_constant, top_y);              //top left
+    }
+
+    virtual int get_digit_length()
+    {
+        return digit_length;
+    }
+
+    ~Zero()
+    {
+        delete[] draw_points;
     }
 };
